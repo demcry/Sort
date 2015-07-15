2c05d290a5c384b1cdedce1d4c3f48afb6035706
LilyaSamedova <lilya68050@mail.ru>
2015-07-15 20:12:20 +0300

    Update case_sens_with_r_strcmp.c

--- src/case_sens_with_r_strcmp.c
+++ src/case_sens_with_r_strcmp.c
@@ -1,10 +1,10 @@
 int case_sensitive_strcmp(const void* a1, const void* b1)
 {
-    char* a = (char*)a1;
-    char* b = (char*)b1;
+	char* a = (char*)a1;
+	char* b = (char*)b1;
     
-    int n=strcmp(a,b);
-    return n*(-1);
+	int n=strcmp(a,b);
+	return n*(-1);
 }
 
 int r_case_sensitive_strcmp(const void* a1, const void* b1)
